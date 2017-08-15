b0aec01f59a930bf6e44cf820f4cb6341a5b1e4f
Sutrebuh <Sutrebuh@users.noreply.github.com>
2017-08-15 17:09:48 +0200

    beginning

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-This should become a python file.
+from nptdms import TdmsFile
+
+tdms_file = TdmsFile("path_to_file.tdms")
+channel = tdms_file.object('Group', 'Channel1')
+data = channel.data
+time = channel.time_track()
+# do stuff with data
+
+
+https://nptdms.readthedocs.io/en/latest/
